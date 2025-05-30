4ea86a827e78dd9f27745e44e5a0a1335a0b968d
Mariam Hasanat <hasanattmariam@gmail.com>
2025-05-30 22:43:00 +0300

    feat: create Candy class

---
+++ Candy.py
@@ -0,0 +1,17 @@
+from Product import Product
+class Candy(Product):
+    def __init__(self, name, price, flavor):
+        super().__init__(name, price)
+        self.__flavor = flavor
+
+    def get_flavor(self):
+        return self.__flavor
+
+    def set_flavor(self, flavor):
+        if not flavor:
+            raise ValueError("Flavor cannot be empty")
+        self.__flavor = flavor
+
+    def display_info(self):
+        product_info = super().display_info()
+        return f"{product_info}, Flavor: {self.__flavor}"
